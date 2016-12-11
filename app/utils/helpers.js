a44f72177f4d2916c4af4f0197c3ec89aba525bd
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-10 20:12:35 -0500

    helpers changes complete

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -8,11 +8,8 @@ function getUserInfo(username) {
   return axios.get(`https://api.github.com/users/${username}`);
 }
 
-const helpers = {
-  getGithubInfo(username) {
+export default function getGithubInfo(username) {
     return axios.all([getRepos(username), getUserInfo(username)])
       .then((arr) => ({repos: arr[0].data, bio: arr[1].data}))
   }
 }
-
-export default helpers
