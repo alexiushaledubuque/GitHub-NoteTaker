3fff9f35dab2f09697c696aead8569bec1e78f5e
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-10 19:20:50 -0500

    Cleaning up Search

--- public/bundle.js
+++ public/bundle.js
@@ -26539,40 +26539,6 @@
 
 	exports.default = SearchGithub;
 
-	// import React from 'react';
-	// import { Router, browserHistory } from 'react-router';
-	//
-	// class SearchGithub extends React.Component {
-	//   getRef(ref){
-	//     this.usernameRef = ref;
-	//   }
-	//   handleSubmit(){
-	//     const username = this.usernameRef.value;
-	//     this.usernameRef.value = '';
-	//     this.props.browserHistory.push(null, "/profile/" + username)
-	//   }
-	//   render(){
-	//     return (
-	//       <div className="col-sm-12">
-	//         <form onSubmit={this.handleSubmit}>
-	//           <div className="form-group col-sm-7">
-	//             <input type="text" className="form-control" ref={this.getRef} />
-	//           </div>
-	//           <div className="form-group col-sm-5">
-	//             <button type="submit" className="btn btn-block btn-primary">Search Github</button>
-	//           </div>
-	//         </form>
-	//       </div>
-	//     )
-	//   }
-	// }
-	//
-	// SearchGithub.PropTypes = {
-	//   history: React.PropTypes.object.isRequired
-	// }
-	//
-	// export default SearchGithub;
-
 /***/ },
 /* 236 */
 /***/ function(module, exports, __webpack_require__) {
