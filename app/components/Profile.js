7297d08b152ecddfcab5835548299bfef3fbc949
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-10 21:47:34 -0500

    ES6 Profile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,54 +1,51 @@
 import React from 'react'
-import { Router } from 'react-router'
 import Repos from './GitHub/Repos'
 import UserProfile from './GitHub/UserProfile'
 import Notes from './Notes/Notes'
-import ReactFireMixin from 'reactfire'
-import Firebase from 'firebase'
 import getGithubInfo from '../utils/helpers'
+import Rebase from 're-base'
 
-const Profile = React.createClass({
-  mixins: [ReactFireMixin],
-  getInitialState () {
-    return {
+class Profile extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
       notes: [],
       bio: {},
       repos: []
     }
-  },
+  }
   componentDidMount () {
-    this.ref = new Firebase('http://github-notetaker-ce9a6.firebaseio.com/')
     this.init(this.props.params.username)
-  },
+  }
   componentWillReceiveProps (nextProps) {
-    this.unbind('notes')
     this.init(nextProps.params.username)
-  },
+  }
   componentWillUnmount () {
-    this.unbind('notes')
-  },
+
+  }
   init (username) {
     const childRef = this.ref.child(username)
-    this.bindAsArray(childRef, 'notes')
+  this.bindAsArray(childRef, 'notes')
 
     getGithubInfo(username)
       .then(function(data){
-        this.setState({
+      this.setState({
           bio: data.bio,
           repos: data.repos
-        })
+      })
       }.bind(this))
-  },
+  }
   handleAddNote (newNote) {
-    this.ref.child(username).child(this.state.notes.length).set(newNote)
-  },
+
+  }
   render () {
     return (
       <div className="row">
         <div className="col-md-4">
           <UserProfile username={this.props.params.username} bio={this.state.bio} />
         </div>
-        <div className="col-md-4">
+      <div className="col-md-4">
           <Repos username={this.props.params.username} repos={this.state.repos}/>
         </div>
         <div className="col-md-4">
@@ -59,6 +56,7 @@ const Profile = React.createClass({
       </div>
     )
   }
-})
 
-module.exports = Profile
+}
+
+export default Profile
