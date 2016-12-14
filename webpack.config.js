28c76dfafb0f9716646d324d08549977e20f21a2
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-14 12:50:27 -0500

    Resolving webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: './app/App.js',
   output: {
-    path: path.join(__dirname, '/public')
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
   resolve: {
