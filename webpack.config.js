935337110c3c712a25e07bad0b3232e6385c30cf
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-14 12:38:41 -0500

    Configuring Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path')
 
 module.exports = {
+  context: __dirname,
   entry: './app/App.js',
   output: {
-    filename: 'public/bundle.js'
+    path: path.join(__dirname, '/public')
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
