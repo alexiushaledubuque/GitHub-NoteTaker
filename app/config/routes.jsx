ee765eb2074d8ae9aeae5962b795e6412a7ae55f
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-15 11:47:54 -0500

    Resolving routes issues

--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -2,23 +2,11 @@ import React from 'react'
 import Main from '../components/Main'
 import Home from '../components/Home'
 import Profile from '../components/Profile'
-import NotFound from '../components/NotFound'
 import { Route, IndexRoute } from 'react-router'
 
-// export default (
-//   <Route path='/' component={Main}>
-//     <Route path='/profile/:username' component={Profile} />
-//     <IndexRoute component={Home} />
-//     <Route path="*" component={NotFound} />
-//   </Route>
-// )
-
-const routes = (
+export default (
   <Route path='/' component={Main}>
     <Route path='/profile/:username' component={Profile} />
     <IndexRoute component={Home} />
-    <Route path="*" component={NotFound} />
   </Route>
 )
-
-export default routes
