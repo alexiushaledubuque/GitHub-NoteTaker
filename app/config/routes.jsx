2187c9e3a71668e361b59fd884a0e668960775dc
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-15 10:27:25 -0500

    Resolving server issues

--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import Main from '../components/Main'
 import Home from '../components/Home'
 import Profile from '../components/Profile'
+import NotFound from '../components/NotFound'
 import { Route, IndexRoute } from 'react-router'
 
-export default (
+// export default (
+//   <Route path='/' component={Main}>
+//     <Route path='/profile/:username' component={Profile} />
+//     <IndexRoute component={Home} />
+//     <Route path="*" component={NotFound} />
+//   </Route>
+// )
+
+const routes = (
   <Route path='/' component={Main}>
     <Route path='/profile/:username' component={Profile} />
     <IndexRoute component={Home} />
+    <Route path="*" component={NotFound} />
   </Route>
 )
+
+export default routes
--- public/bundle.js
+++ public/bundle.js
@@ -26406,16 +26406,31 @@
 
 	var _Profile2 = _interopRequireDefault(_Profile);
 
+	var _NotFound = __webpack_require__(277);
+
+	var _NotFound2 = _interopRequireDefault(_NotFound);
+
 	var _reactRouter = __webpack_require__(178);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	exports.default = _react2.default.createElement(
+	// export default (
+	//   <Route path='/' component={Main}>
+	//     <Route path='/profile/:username' component={Profile} />
+	//     <IndexRoute component={Home} />
+	//     <Route path="*" component={NotFound} />
+	//   </Route>
+	// )
+
+	var routes = _react2.default.createElement(
 	  _reactRouter.Route,
 	  { path: '/', component: _Main2.default },
 	  _react2.default.createElement(_reactRouter.Route, { path: '/profile/:username', component: _Profile2.default }),
-	  _react2.default.createElement(_reactRouter.IndexRoute, { component: _Home2.default })
+	  _react2.default.createElement(_reactRouter.IndexRoute, { component: _Home2.default }),
+	  _react2.default.createElement(_reactRouter.Route, { path: '*', component: _NotFound2.default })
 	);
+
+	exports.default = routes;
 
 /***/ },
 /* 234 */
@@ -30530,6 +30545,66 @@
 	module.exports = firebase.messaging;
 
 	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ },
+/* 277 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactRouter = __webpack_require__(178);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var NotFound = function (_React$Component) {
+	  _inherits(NotFound, _React$Component);
+
+	  function NotFound() {
+	    _classCallCheck(this, NotFound);
+
+	    return _possibleConstructorReturn(this, (NotFound.__proto__ || Object.getPrototypeOf(NotFound)).apply(this, arguments));
+	  }
+
+	  _createClass(NotFound, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'div',
+	        { className: 'not-found' },
+	        _react2.default.createElement(
+	          'h2',
+	          null,
+	          'Page not found'
+	        ),
+	        _react2.default.createElement(
+	          'p',
+	          null,
+	          _react2.default.createElement(_reactRouter.Link, { to: '/' })
+	        )
+	      );
+	    }
+	  }]);
+
+	  return NotFound;
+	}(_react2.default.Component);
+
+	exports.default = NotFound;
 
 /***/ }
 /******/ ]);
