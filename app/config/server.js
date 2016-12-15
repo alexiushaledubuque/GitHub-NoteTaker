6491c7b768d520ff1d66abb34c6665a7ee14256c
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-15 12:51:25 -0500

    Server.js

--- app/config/server.js
+++ app/config/server.js
@@ -8,35 +8,40 @@ const Express = require('express')
 const React = require('react')
 const { renderToString } = require ('react-dom/server')
 const { match, RouterContext } = require('react-router') 
-const routes = require('./routes.jsx')
+// const routes = require('./routes.jsx')
+const routes = require('./routes')
+
 
 // initialize the server and configure support for ejs templates
 const app = new Express()
 const server = new Server(app)
 
 app.set('view engine', 'ejs')
+
 app.set('views', path.join(__dirname, 'views'))
+
 
 // define the folder that will be used for static assets
 app.use(Express.static(path.join(__dirname, 'public')))
 
+
 // universal routing and rendering
-app.use((req, res) => {
+app.get((req, res) => {
 
   match(
 
     { routes, location: req.url }, 
 
-    (err, redirectionLocation, renderProps) => {
+    (err, redirectLocation, renderProps) => {
     
     // in case of error display the error message
     if (err) {
-      res.status(500).send(error.message)
+      res.status(500).send(err.message)
     } 
 
     // in case of redirect propagate the redirect to the browser
-    if (redirectionLocation) {
-      return res.redirect(302, redirectionLocation.pathname + redirectionLocation.search)  
+    if (redirectLocation) {
+      return res.redirect(302, redirectLocation.pathname + redirectLocation.search)  
     } 
 
     // generate the React markup for the current route
@@ -44,14 +49,13 @@ app.use((req, res) => {
 
     if (renderProps) {
       // if the current route matched we have renderProps
-      body = ReactDOMServer.renderToString(
-        React.createElement(RouterContext, renderProps)
-        )
-      res.status(200)
-    } else {
-      // otherwise we can render a 404 page
-      res.status(404)
-    }
+      body = renderToString(<RouterContext {...renderProps} />)
+      
+    } 
+    // else {
+    //   // otherwise we can render a 404 page
+    //   res.status(404)
+    // }
 
     // render the index template with the embedded React markup
     return res.render('index', { body })
