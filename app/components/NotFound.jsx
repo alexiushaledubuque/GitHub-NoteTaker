053f07a916f84047af808516fa75c4d38b310146
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-15 10:08:13 -0500

    Rename component

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -6,6 +6,7 @@ export default class NotFound extends React.Component {
   render () {
     return (
       <div className="not-found">
+        <h2>Page not found</h2>
         <p>
           <Link to="/"></Link>
         </p>
